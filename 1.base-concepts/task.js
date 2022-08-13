27172514fa2795bd360957ae524df683e5d5889a
Alexandr Vasnev <sanek.vasnev@gmail.com>
2022-08-13 06:29:46 +0300

    change 'return'

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,18 +15,17 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
   let numPercent = parseInt(percent);
   let numContribution = parseInt(contribution);
   let numAmount = parseInt(amount);
 
   //Проверка типа данных
   if(isNaN(numPercent)) {
-    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
   }else if(isNaN(numContribution)) {
-    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
   }else if(isNaN(numAmount)) {
-    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
   }
 
   let today = new Date();
@@ -41,11 +40,10 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let payment = numAmount * (numPercent + (numPercent / (((1 + numPercent)**nomberOfMonths) - 1)));
 
   if(numContribution < numAmount) {
-    totalAmount = +(nomberOfMonths * payment).toFixed(2);
+    console.log(+(nomberOfMonths * payment).toFixed(2));
+    return +(nomberOfMonths * payment).toFixed(2);
   } else {
-    totalAmount = 0
+    console.log(0);
+    return 0
   }
-  
-  console.log(totalAmount);
-  return totalAmount;
 }
