0a607cab3e211513559054a5cf751c7673bb7964
Aleksandr Vasnev <101241017+Alexandr7944@users.noreply.github.com>
2022-08-16 13:20:51 +0300

    Homework array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
+	
+  if(arr1.length === arr2.length) {
+	result = arr1.every((elem, index) => elem === arr2[index]);
+  } else {
+	result = false;
+  }
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10);
 
   return resultArr; // array
 }
