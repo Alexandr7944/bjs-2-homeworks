5669d22a4e35714857df9de6e40ca09662019179
Aleksandr Vasnev <101241017+Alexandr7944@users.noreply.github.com>
2022-08-16 14:10:56 +0300

    function change

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,19 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-	
-  if(arr1.length === arr2.length) {
-	result = arr1.every((elem, index) => elem === arr2[index]);
-  } else {
-	result = false;
-  }
-
-  return result;
+  return arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  resultArr = arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10);
-
-  return resultArr; // array
+	return arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10);   
 }
