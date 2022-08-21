686a3eabddaa7126c194e1aec9d12160762f6339
Alexandr Vasnev <sanek.vasnev@gmail.com>
2022-08-21 13:16:29 +0300

    hommeworks objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,27 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  this.marks === undefined ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...mark) {
+  this.marks === undefined ? this.marks = [...mark] : this.marks.push(...mark);
+}
+
+Student.prototype.getAverage = function() {
+  this.getAverage = (this.marks.reduce((acc, item) => acc + item, 0)) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
