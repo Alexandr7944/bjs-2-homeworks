2da9dcbe44b047e16e4dabca2b67e2f767326cda
Alexandr Vasnev <sanek.vasnev@gmail.com>
2022-09-01 20:06:51 +0300

    change class Library giveBookByName()

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -88,7 +88,7 @@ class Library{
     giveBookByName(bookName) {
         for(let i = 0; i < this.books.length; i++) {
             if(this.books[i].name === bookName) {
-                return this.books.splice([i], 1);
+                return this.books.splice([i], 1)[0];
             }
         }
         return null;
@@ -133,7 +133,7 @@ class Student{
             summ += this.subjectNames[index].marks.reduce((acc, item) => acc + item, 0);
             number += this.subjectNames[index].marks.length;
         }
-        return +(summ / number).toFixed(2);
+        return +((summ / number).toFixed(2));
     }
 
     exclude(reason) {
