c6241dc8538660e2472675b1e4e86d22c1a559e8
Alexandr Vasnev <sanek.vasnev@gmail.com>
2022-09-15 12:03:28 +0300

    homeworks decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,86 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+
+  function wrapper(...arr) {
+    let hash = arr.join(',');
+    let objectInCache = cache.find((item) => {
+      if(item.hash === hash) {
+        return item;
+      }
+    })
+
+    if(objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...arr);
+    cache.unshift({
+      hash: hash,
+      result: result
+    });
+
+    if(cache.length > 5){
+      cache.pop();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper
 }
+
+
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timeoutId = false;
+  function wrapper(...args) {
+    let result = func(...args);
+    if(timeoutId == false) {
+      timeoutId = null;
+      console.log('Результат первичный ' + result);
+      return result
+    } else {
+      if(timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+        console.log('Результат после таймаута ' + result);
+        return result;
+      }, 2000)
+    }
+  }
+  return wrapper
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timeoutId = false;
+  function wrapper(...args) {
+    wrapper.count.allCount++;
+    let result = func(...args);
+    if(timeoutId == false) {
+      timeoutId = null;
+      console.log('Результат первичный ' + result);
+      wrapper.count.resultCount++;
+      return result
+    } else {
+      if(timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+        console.log('Результат после таймаута ' + result);
+        wrapper.count.resultCount++;
+        return result;
+      }, 2000)
+    }
+  }
+  wrapper.count = {
+    allCount: 0,
+    resultCount: 0,
+  };
+
+  return wrapper
 }
